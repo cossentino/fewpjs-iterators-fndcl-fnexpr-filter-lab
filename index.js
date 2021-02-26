6eccea3a355c616c6a2bafa9dce4aa9b281fc157
Ian Cossentino <icossentino@gmail.com>
2021-02-26 16:52:58 -0500

    commit tree 99eeb1458e4e76da7d6a2c1d147bce196d4e95ee

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+
+
+
+
+
+
+
+function findMatching(array, str) {
+  let matches = array.filter(driver => driver.toLowerCase() === str.toLowerCase())
+  return matches
+}
+
+
+function fuzzyMatch(array, str) {
+  let length = str.length
+  let matches = array.filter(driver => driver.slice(0, length).toLowerCase() === str.toLowerCase())
+  return matches
+}
+
+
+function matchName(array, str) {
+  let matches = array.filter(driver => driver['name'].toLowerCase() === str.toLowerCase())
+  return matches
+}
